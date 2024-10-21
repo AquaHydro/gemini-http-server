e0d3c6edd711655b70d399392d1b88a83bb266c4
waterhydroxyl <contact@yiliang.me>
2024-10-21 11:58:56 +0800

    Update index.ts

--- model/index.ts
+++ model/index.ts
@@ -5,6 +5,6 @@ dotenv.config();
 
 const genAI = new GoogleGenerativeAI(process.env.Gemini_Key);
 
-const model = genAI.getGenerativeModel({ model: 'gemini-1.5-flash' });
+const model = genAI.getGenerativeModel({ model: 'gemini-1.5-flash-002' });
 
 export default model;
