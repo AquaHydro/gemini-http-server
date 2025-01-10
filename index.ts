caae3f63d341865ce0a316a28ca57764d221d306
AquaHydro <contact@yiliang.me>
2025-01-10 14:15:16 +0800

    Update index.ts

--- index.ts
+++ index.ts
@@ -12,7 +12,7 @@ const router = new Router();
 app.on('error', errorHandler);
 app.use(cors({
   origin: (ctx: Context) => {
-    const allowedOrigins = ['https://www.ilikestudy.cn', 'https://blog.yiliang.me'];
+    const allowedOrigins = ['https://www.ilikestudy.cn', 'https://blog.yiliang.me', 'https://gemini.ilikestudy.cn'];
     if (allowedOrigins.includes(ctx.request.header.origin || '')) {
       return ctx.request.header.origin;
     }
