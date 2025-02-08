93efa12119a679145b959e16b581f7998cfd3f58
AquaHydro <contact@yiliang.me>
2025-02-08 15:58:25 +0800

    Update index.ts

--- model/index.ts
+++ model/index.ts
@@ -5,6 +5,6 @@ dotenv.config();
 
 const genAI = new GoogleGenerativeAI(process.env.Gemini_Key);
 
-const model = genAI.getGenerativeModel({ model: 'gemini-1.5-flash' });
+const model = genAI.getGenerativeModel({ model: 'gemini-2.0-flash' });
 
 export default model;
